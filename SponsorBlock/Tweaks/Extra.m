23a26c6bb6ca5c1d0096a449b3a9d24ddbe499be
hugeBlack <wwkq03@outlook.com>
2026-05-31 14:42:11 +0800

    remove BAPIAdV1SourceContentDto hooks as the original tweak already has it

--- SponsorBlock/Tweaks/Extra.m
+++ SponsorBlock/Tweaks/Extra.m
@@ -22,22 +22,7 @@
 
 @end
 
-@interface BAPIAdV1SourceContentDto : GPBMessage
-@property (nonatomic) BOOL hasAdContent;
-@end
-
-id (*orig_BAPIAdV1SourceContentDto_initWithData_extensionRegistry_error)(id self, SEL, id data, id registry, id* error) = nil;
-id hook_BAPIAdV1SourceContentDto_initWithData_extensionRegistry_error(id self, SEL sel, id data, id registry, id* error) {
-    BAPIAdV1SourceContentDto* ret = orig_BAPIAdV1SourceContentDto_initWithData_extensionRegistry_error(self, sel, nil, registry, error);
-    return ret;
-}
-
 void initExtraHooks(void) {
     // fix app not displayed in NowPlaying center due to PhotosUI switching AVAudioSession's category to AVAudioSessionCategoryAmbient
     swizzle(AVAudioSession.class, @selector(setCategory:mode:routeSharingPolicy:options:error:), @selector(hook_setCategory:mode:routeSharingPolicy:options:error:));
-    
-    // remove iPad playlist ad
-    JRSwizzleInstanceMethod(objc_getClass("BAPIAdV1SourceContentDto"), @selector(initWithData:extensionRegistry:error:),
-                            (IMP)hook_BAPIAdV1SourceContentDto_initWithData_extensionRegistry_error,
-                            (IMP*)&orig_BAPIAdV1SourceContentDto_initWithData_extensionRegistry_error);
 }
